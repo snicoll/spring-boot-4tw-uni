a04d79b2f3680aeee0f5f8dd9420e04c64f61790
Stephane Nicoll <snicoll@pivotal.io>
2015-08-13 10:44:13 +0200

    Cache demonstration
    While our library has obviously some sort of caching of the Maven
    meta-data we can see in the logs that every time we need to diff two
    versions, it is called to recompute the model.
    
    We can add the cache starter and `@EnableCaching` on our app and it will
    just work out-of-the-box. Since we have no caching provider available,
    Spring Boot uses a simple implementation based on concurrent hash maps.
    
    We can also show how we can integrate a real cache library. For the sake
    of the demo, we're going to use ehcache 3 and create a
    JCacheManagerCustomizer that creates our `diffs` cache. Since the model
    may refer to a SNAPSHOT version, we need to expire the entry after some
    time. So we create a custom configuration that does that.
    
    As we have the actuator now, we can see that the `/metrics` endpoint now
    shows hit/miss stats of our cache.

--- spring-boot-4tw-web/src/main/java/demo/config/Application.java
+++ spring-boot-4tw-web/src/main/java/demo/config/Application.java
@@ -1,6 +1,9 @@
 package demo.config;
 
 import java.util.Random;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
@@ -9,13 +12,16 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.jmx.export.MBeanExporter;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
@@ -44,6 +50,17 @@ public class Application {
 						.withDetail("Ooops", 42)
 						.build();
 			}
+		};
+	}
+
+	@Bean
+	public JCacheManagerCustomizer cacheManagerCustomizer() {
+		return cm -> {
+			cm.createCache("diffs", new MutableConfiguration<>()
+					.setExpiryPolicyFactory(CreatedExpiryPolicy
+							.factoryOf(Duration.ONE_HOUR))
+					.setStoreByValue(false)
+					.setStatisticsEnabled(true));
 		};
 	}
 
