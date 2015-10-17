d7f7571bbe7f64abf300834ba5f439d86efcc2e2
Stephane Nicoll <snicoll@pivotal.io>
2015-10-17 10:20:12 +0200

    Cross Origin demonstration
    Let's start our separate versions provider on port 9090 and change the
    client to connect there instead on the application itself. Our separate
    app does not manage cross origin requests by default so the browser
    rejects that resource. Spring Framework now has a comprehensive
    CORS support.
    
    To allow request from any origin to our new `SpringBootVersionController`
    controller, we can just annotate it with `@CrossOrigin`.

--- spring-boot-4tw-versions-provider/src/main/java/demo/version/SpringBootVersionController.java
+++ spring-boot-4tw-versions-provider/src/main/java/demo/version/SpringBootVersionController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin
 public class SpringBootVersionController {
 
 	private final SpringBootVersionService bootVersionService;
